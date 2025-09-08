0dd575abb1136838a85c69cf945fb23518224f86
Peter Osztodi <osztodipeter@gmail.com>
2025-09-08 17:54:57 +0100

    applied

--- modules/snowflake/account/70-resource-monitor.tf
+++ modules/snowflake/account/70-resource-monitor.tf
@@ -5,7 +5,7 @@ resource "snowflake_resource_monitor" "overall_credit_monitor" {
   credit_quota = 10000
 
   frequency       = "MONTHLY"
-  start_timestamp = substr(replace(timeadd(timestamp(), "1h"), "T", " "), 0, 16)
+  start_timestamp = "2025-09-08 18:00"
   end_timestamp   = "2035-12-31 00:00"
 
   notify_triggers           = [50, 60, 70, 80, 90]
